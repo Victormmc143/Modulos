e2fd8a7a4af3349c889eb45840c013793f7cbd5e
vmercado <victormmc143@gmail.com>
2022-06-17 20:41:48 -0500

    CAmbios en los modulos

--- __manifest__.py
+++ __manifest__.py
@@ -1,14 +1,15 @@
 {
-    'name': 'Historia Clinica',
+    'name': 'Modulos Curso odoo',
     'version': '1',
-    'description': 'Historia Historia_Clinica',
+    'description': 'Modulo Curso odoo',
     'category': 'Account',
     'author': 'VICTOR MERCADO',
     'license': 'AGPL-3',
     'depends': ['base', 'account'],
     'data': [
         'security/ir.model.access.csv',
-        'view/consecutivos_view.xml'
+        'view/consecutivos_view.xml',
+        'view/paciente_view.xml'
     ],
     'installable': True,
     'auto_install': False
