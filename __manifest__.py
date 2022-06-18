5be8ef89d64be92523b6e0fbaa12efa551718088
vmercado <victormmc143@gmail.com>
2022-06-17 20:31:45 -0500

    modulos del curso de odoo

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Historia Clinica',
+    'version': '1',
+    'description': 'Historia Historia_Clinica',
+    'category': 'Account',
+    'author': 'VICTOR MERCADO',
+    'license': 'AGPL-3',
+    'depends': ['base', 'account'],
+    'data': [
+        'security/ir.model.access.csv',
+        'view/consecutivos_view.xml'
+    ],
+    'installable': True,
+    'auto_install': False
+}
